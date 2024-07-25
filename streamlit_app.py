fb4e4a1d42d5fac4510e9205eaaaae4d80733a77
Abhi Saini <110130910+sfc-gh-asaini@users.noreply.github.com>
2024-07-25 19:00:24 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,5 @@
 import streamlit as st
 
-st.title('Hello NY Meetup')
+st.title('Hello NY Meetup :)')
 
 st.write('This is the landing page for the app. Click in the left sidebar to open other pages in the app')
