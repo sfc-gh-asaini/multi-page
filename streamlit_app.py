98a744a550cca9d3d61b198658e6b738bb1543bd
Abhi Saini <110130910+sfc-gh-asaini@users.noreply.github.com>
2024-07-25 17:59:39 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,5 @@
 import streamlit as st
 
-st.title('Multi-page app demo')
+st.title('Hello NY Meetup')
+
 st.write('This is the landing page for the app. Click in the left sidebar to open other pages in the app')
